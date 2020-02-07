f4a98964acc665897941ec24831aa351044640d5
Rafael Franco <rafaelfr@outlook.com>
2020-02-07 14:44:33 -0300

    chore: update jsx-a11y rule

--- packages/eslint-config-1e3-react/src/jsx.js
+++ packages/eslint-config-1e3-react/src/jsx.js
@@ -3,7 +3,7 @@ const { OFF, ERROR } = require('./constants')
 module.exports = {
   'jsx-a11y/aria-props': ERROR,
   'jsx-a11y/heading-has-content': OFF,
-  'jsx-a11y/href-no-hash': ERROR,
+  'jsx-a11y/anchor-is-valid': ERROR,
   'jsx-a11y/label-has-for': ERROR,
   'jsx-a11y/mouse-events-have-key-events': ERROR,
   'jsx-a11y/role-has-required-aria-props': ERROR,
